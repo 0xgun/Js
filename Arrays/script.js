b481abc9a67fe6f07c71bb4c170798cccb236856
mganeshan29 <ganeshanmadesh36@outlook.com>
2022-11-24 01:21:19 +0530

    D4

---
+++ Arrays/script.js
@@ -0,0 +1,34 @@
+// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// for( let i=0;i<months.length;i++){
+//   console.log(months[i]);
+// }
+const names = ['Ganesha', 'Siva', 'Vishnu', 'Durga'];
+//push
+// names.push('Lakshmi');
+// console.log(names);
+// //pop
+// names.pop();
+// console.log(names);
+// names.shift()
+// console.log(names);
+
+// names.unshift('Lakshmi');
+// console.log(names);
+
+// // array splice 
+// names.splice(2,0,'Alice','Gun');
+// console.log(names);
+
+//array slice
+// const newNames = names.slice(1,3);
+// console.log(newNames);
+// names.forEach((names, i)=>{
+//   console.log(`${i + 1} - ${names}`);
+// });
+// console.log(typeof forEach);
+const number =[2,6,7,7]
+let sum=0;
+number.forEach((numbers)=>{
+  sum+=numbers
+});
+console.log(sum);
---
+++ Strings/script.js
@@ -0,0 +1,46 @@
+// // const single='This is a string';
+// // const double="This is a string";
+// // const backtick=`${2+2}`;
+// // console.log(single);
+// // console.log(double);
+// // console.log(backtick);
+
+// // var name = "Ganesha";
+// // // console.log(name.length);
+// // // console.log(name[name.length-1]);
+// // console.log(name.toUpperCase());
+// // console.log(name.toLowerCase());
+// const hobbies = "playing,reading,watching";
+// const b=hobbies.indexOf("watching");
+// console.log(b);
+
+// const outer=()=>{
+//   const outerVar="Hello";
+//   console.log(outerVar);
+//   const inner=()=>{
+//     const innerVar="World";
+//     console.log(outerVar);
+//     console.log(innerVar);
+//   }
+//   return inner;
+// }
+// // console.log(outerVar);
+// const v=outer();
+// v();
+
+// const init=()=>{
+//   const msg="Learning Js";
+//   const inner=()=>{
+//     console.log(msg);
+//   }
+//   inner();
+// }
+// init();
+
+// const str="Quick brown fox jumps over the lazy dog";
+// console.log(str.split(''));
+const v='test  ';
+// const a=v.split('');
+// console.log((a.reverse().join('')));
+console.log(v.repeat(3));
+console.log(v.trim());
